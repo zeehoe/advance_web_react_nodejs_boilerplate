87a3ac06cac87e66b41192dd889a9433c6d503ea
zeehoe <zeehoe95@gmail.com>
2022-07-20 23:18:38 +0800

    avatar setting

--- public/src/App.js
+++ public/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./pages/login"
 import Register from "./pages/register"
+import SetAvatar from "./pages/setAvatar"
 import Home from "./pages/home"
 
 function App() {
@@ -10,9 +11,10 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
         <Route path="/home" element={<Home />}/>
       </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
     </>    
   );
 }
